c17bf31bd686433fbe260aca74372a1b4056f51d
Ionut Sauciuc <ionut.sauciuc2003@gmail.com>
2021-08-08 00:21:05 +0300

    added token model, not giving it on registering, updated dashboard

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import Group
 
 # Create your models here.
+
+# TOKEN MODEL ---------------------------------------
+# @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
+        # Group.objects.get(name="premium").user_set.add(instance)
