e960e89b17795befb6be10a0f76001885e630a1d
Ionut Sauciuc <ionut.sauciuc2003@gmail.com>
2021-08-17 21:33:39 +0300

    created form for users to enter their key in

--- api/forms.py
+++ api/forms.py
@@ -1,9 +1,23 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import SubscriptionKeys
 
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+
+# checking the form used to register keys
+class KeyRegisterForm(forms.Form):
+    key = forms.CharField(label='Key', max_length=36)
+
+    def clean(self):
+        key = self.cleaned_data.get('key')
+        try:
+            key = SubscriptionKeys.objects.get(key=key)
+            key.delete()
+        except:
+            self.add_error('key', 'The entered key is non-existent!')
+
