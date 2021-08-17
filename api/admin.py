16d3af9467b7cb0f8d0bc1301cb75b354ef1236e
Ionut Sauciuc <ionut.sauciuc2003@gmail.com>
2021-08-17 21:32:53 +0300

    created 2 more models and added them to admin site, for subscriptions and subscriptionkeys

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.contrib.admin.sites import AdminSite
+from api.models import SubscriptionKeys, Subscriptions
 
 # Register your models here.
+admin.site.register(SubscriptionKeys)
+admin.site.register(Subscriptions)
--- api/models.py
+++ api/models.py
@@ -4,8 +4,21 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# model for keys used to add subscriptions
+class SubscriptionKeys(models.Model):
+    key = models.CharField(max_length=36)
+
+# subscription table, containing everyone that has a subscription with the following data: their user, their token and when their subscription started
+# and when it ends
+class Subscriptions(models.Model):
+    user = models.OneToOneField(User, primary_key=True, on_delete=models.CASCADE)
+    token = models.OneToOneField(Token, on_delete=models.CASCADE)
+    subscription_start = models.DateField()
+    subscription_end = models.DateField()
 
 """
 # TOKEN MODEL ---------------------------------------
